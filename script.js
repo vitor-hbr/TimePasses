5128876ff4af2e2cbd6fc23203c3ca70e06686d1
vitor-hbr <53880209+vitor-hbr@users.noreply.github.com>
2019-08-15 16:59:20 -0300

    Organizing functions

--- script.js
+++ script.js
@@ -2,46 +2,54 @@ var morning = { r: 234, g: 250, b: 215};
 var afternoon = { r: 236, g: 165, b: 77};
 var night = { r: 2, g: 2, b: 68};
 
-var StartTime = true;
+var body;
+var msg; 
+var imgs;
 
+var hour;
+var min; 
+var sec;
+var date;
+
+var td;
+var per;
+
+getElements();
+setTimeout(load, 1000);
 
 function load() {
-    var body = window.document.getElementById("body");
-    var msg = window.document.getElementById("msg");
-    var imgs = window.document.querySelectorAll("img");
-    var data = new Date();
-    var hora = data.getHours();
-    var min = data.getMinutes();
-    var sec = data.getSeconds();
-    var td;
-    var per;
-    msg.innerHTML = `Time now is ${hora}:${min}:${sec}`;
-    if (hora <= 12 && hora >= 6) {
-        imgs[2].style.opacity = 0;
-        td = calcdif(hora, min, sec, 0);
-        per = 1 - (td / (7*3600));
-        imgs[0].style.opacity = 1 - per;
-        imgs[1].style.opacity = per;
-        body.style.background = `#${rgbToHex(morning.r*(1-per) + afternoon.r*per, morning.g*(1-per) + afternoon.g*per, morning.b*(1-per) + afternoon.b*per)}`;
-    } else if (hora >= 13 && hora <= 18) {
-        imgs[0].style.opacity = 0;
-        td = calcdif(hora, min, sec, 1);
-        per = 1 - (td / (6*3600));
-        imgs[1].style.opacity = 1 - per;
-        imgs[2].style.opacity = per;
-        body.style.background = `#${rgbToHex(afternoon.r*(1-per) + night.r*per, afternoon.g*(1-per) + night.g*per, afternoon.b*(1-per) + night.b*per)}`;
-    } else {
-        imgs[1].style.opacity = 0;
-        td = calcdif(hora, min, sec, 2);
-        per = 1 - (td / (11*3600));
-        imgs[2].style.opacity = 1 - per;
-        imgs[0].style.opacity = per;
-        body.style.background = `#${rgbToHex(night.r*(1-per) + morning.r*per, night.g*(1-per) + morning.g*per, night.b*(1-per) + morning.b*per)}`;
-    }
-    setTimeout(carregar, 1000);
+    getTime();
+    msg.innerHTML = `Time now is ${hour}:${min}:${sec}`;
+    if (hour <= 12 && hour >= 6)
+        handleColorAndOpacityChange(2, 0, 1, morning, afternoon);
+    else if (hour >= 13 && hour <= 18) 
+        handleColorAndOpacityChange(0, 1, 2, afternoon, night);
+    else 
+        handleColorAndOpacityChange(1, 2, 0, night, morning);
 }
 
-carregar();
+function getElements() {
+    body = window.document.getElementById("body");
+    msg = window.document.getElementById("msg");
+    imgs = window.document.querySelectorAll("img");
+}
+
+function getTime() {
+    date = new Date();
+    hour = date.getHours();
+    min  = date.getMinutes();
+    sec  = date.getSeconds();
+}
+
+function handleColorAndOpacityChange(indexEnd, indexFrom, indexTo, timeFrom, timeTo) {
+    imgs[indexEnd].style.opacity = 0;
+    td = calcdif(hour, min, sec, indexFrom);
+    per = 1 - (td / (7*3600));
+    imgs[indexFrom].style.opacity = 1 - per;
+    imgs[indexTo].style.opacity = per;
+    body.style.background = `#${rgbToHex(timeFrom.r*(1-per) + timeTo.r*per, timeFrom.g*(1-per) +
+        timeTo.g*per, timeFrom.b*(1-per) + timeTo.b*per)}`;
+}
 
 function rgbToHex(R, G, B) { return toHex(R) + toHex(G) + toHex(B) }
 
@@ -53,20 +61,20 @@ function toHex(n) {
         + "0123456789ABCDEF".charAt(n % 16);
 }
 
-function calcdif(hora, min, sec, num) {
+function calcdif(hour, min, sec, timeFrom) {
     var t = 0;
-    switch (num) {
+    switch (timeFrom) {
         case 0:
-            t = (3600*13) - (hora*3600 + min*60 + sec);
+            t = (3600*13) - (hour*3600 + min*60 + sec);
             break;
         case 1:
-            t = (3600*19) - (hora*3600 + min*60 + sec);
+            t = (3600*19) - (hour*3600 + min*60 + sec);
             break;
         case 2:
-            if(hora>12)
-                t = (3600*hora) - ((24-hora+5)*3600 + min*60 + sec);
+            if(hour>12)
+                t = (3600*hour) - ((24-hour+5)*3600 + min*60 + sec);
             else
-                t = (3600*6) - (hora*3600 + min*60 + sec);
+                t = (3600*6) - (hour*3600 + min*60 + sec);
             break;
         default:
             t = 0;
